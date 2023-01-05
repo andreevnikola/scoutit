08d59b56ff20ef690d32f719e617c82650335204
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-05 23:09:16 +0200

    configged prettier

--- scout-it/src/app/core/header/header.component.ts
+++ scout-it/src/app/core/header/header.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  search = faMagnifyingGlass;
 
+  constructor() {}
 }
