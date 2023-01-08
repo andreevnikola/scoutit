df0a58f9375880b983f4cd4f60c8a10964d2a4b8
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-08 15:08:29 +0200

    font-awesome successfully implemented, added images

--- scout-it/src/app/core/core.module.ts
+++ scout-it/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HeaderComponent } from './header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [LandingPageComponent, HeaderComponent],
-  imports: [CommonModule],
-  exports: [HeaderComponent],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [HeaderComponent, LandingPageComponent],
 })
 export class CoreModule {}
