f6df9e8021e77280ee78bc8ee5445b83b00ce84b
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-09 14:50:36 +0200

    govnari mnogu picki malku

--- scout-it/tailwind.config.js
+++ scout-it/tailwind.config.js
@@ -1,21 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
       boxShadow: {
-        "custom": "5px 10px 15px rgba(123, 184, 123, 0.25)"
+        custom: '5px 10px 15px rgba(123, 184, 123, 0.25)',
       },
       fontFamily: {
-        "jaldi": ["Jaldi"]
+        jaldi: ['Jaldi'],
       },
       borderWidth: {
-        "1": "1px"
+        1: '1px',
       },
       width: {
-        "half_with_padding": "calc(50% - 4px)"
-      }
+        half_with_padding: 'calc(50% - 4px)',
+      },
+      screens: {
+        xs: { max: '768px' },
+      },
     },
   },
+
   plugins: [],
 };
