79257fd66936683ea256d7bb34b5bb08b732e882
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-16 22:10:37 +0200

    basic 404 skeleton

--- scout-it/src/app/errors/not-found/not-found.component.ts
+++ scout-it/src/app/errors/not-found/not-found.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.css'],
 })
 export class NotFoundComponent {
+  search = faSearch;
 
+  constructor() {}
 }
