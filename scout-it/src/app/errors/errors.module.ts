eec48b77c69d5db098f8620bd4c7f683add36544
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-16 22:10:31 +0200

    basic 404 skeleton

--- scout-it/src/app/errors/errors.module.ts
+++ scout-it/src/app/errors/errors.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
-
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    NotFoundComponent,
-    ServerErrorComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [NotFoundComponent, ServerErrorComponent],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [NotFoundComponent, ServerErrorComponent],
 })
-export class ErrorsModule { }
+export class ErrorsModule {}
