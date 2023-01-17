a99d1ad5669612d2caa1388a751f48009fc6124c
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-17 19:26:45 +0200

    properly working hamburger

--- scout-it/src/app/core/header/header.component.ts
+++ scout-it/src/app/core/header/header.component.ts
@@ -10,4 +10,16 @@ export class HeaderComponent {
   search = faMagnifyingGlass;
 
   constructor() {}
+
+  toggleHamburger() {
+    let navbar = document.querySelector<HTMLElement>('#navbar');
+    let header = document.querySelector<HTMLElement>('#header');
+
+    if (navbar!.style.display === 'block') {
+      navbar!.style.display = 'none';
+    } else {
+      navbar!.style.display = 'block';
+      header!.style.flexDirection = 'column';
+    }
+  }
 }
