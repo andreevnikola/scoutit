05c9a0a488d819c979cd551a494f1b217ef188b4
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-24 23:39:59 +0200

    created modal component

---
+++ scout-it/src/app/profile/public-profile/profile-modal/profile-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-modal',
+  templateUrl: './profile-modal.component.html',
+  styleUrls: ['./profile-modal.component.css']
+})
+export class ProfileModalComponent {
+
+}
