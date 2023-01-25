97774bc31aa1e67ffb5f16737fb5c18b3c8476e2
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-25 23:34:53 +0200

    I can't even test the function, fuck it

--- scout-it/src/app/auth/authenticate.service.ts
+++ scout-it/src/app/auth/authenticate.service.ts
@@ -8,60 +8,42 @@ import { HttpClient } from '@angular/common/http';
 export class AuthenticateService {
   constructor(private httpClient: HttpClient) {}
 
-  registerAccount(
-    phone_starting: string,
-    phone: string,
-    mail: string,
-    username: string,
-    firstname: string,
-    lastname: string,
-    password: string,
-    type: string
-  ) {
-    return this.httpClient.post<IRegistered>(
-      'http://localhost:8080/api/users/register',
-      {
-        username: username,
-        pass: password,
-        fullname: firstname + ' ' + lastname,
-        mail: mail,
-        phone: phone_starting + phone,
-        type: type,
-      }
-    );
+  registerAccount(phone_starting: string, phone: string, mail: string, username: string, firstname: string, lastname: string, password: string, type: string) {
+    return this.httpClient.post<IRegistered>('http://localhost:8080/api/users/register', {
+      username: username,
+      pass: password,
+      fullname: firstname + ' ' + lastname,
+      mail: mail,
+      phone: phone_starting + phone,
+      type: type,
+    });
   }
 
-  signIn(
-    phone_starting: string,
-    phone: string,
-    password: string,
-    type: string
-  ) {
-    return this.httpClient.post<IUser>(
-      'http://localhost:8080/api/users/login',
-      {
-        pass: password,
-        phone: phone_starting + phone,
-        type: type,
-      }
-    );
+  signIn(phone_starting: string, phone: string, password: string, type: string) {
+    return this.httpClient.post<IUser>('http://localhost:8080/api/users/login', {
+      pass: password,
+      phone: phone_starting + phone,
+      type: type,
+    });
   }
 
   verifyMail(key: string, code: string) {
-    return this.httpClient.get<any>(
-      `http://localhost:8080/api/users/verify/${key}/${code}`
-    );
+    return this.httpClient.get<any>(`http://localhost:8080/api/users/verify/${key}/${code}`);
   }
 
   confirmMail(key: string) {
-    return this.httpClient.get<any>(
-      `http://localhost:8080/api/users/confirm/${key}`
-    );
+    return this.httpClient.get<any>(`http://localhost:8080/api/users/confirm/${key}`);
   }
 
   username: string | null = sessionStorage.getItem('username');
 
   get isLogged() {
     return this.username !== null;
+  }
+
+  image: string | null = sessionStorage.getItem('profile_picture');
+
+  get profilePic() {
+    return this.image;
   }
 }
