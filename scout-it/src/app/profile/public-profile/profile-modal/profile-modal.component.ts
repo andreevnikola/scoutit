62cc6c0d4b053112f7e5aa783d1923e365b3cd2b
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-25 23:38:52 +0200

    started some work on the modal

--- scout-it/src/app/profile/public-profile/profile-modal/profile-modal.component.ts
+++ scout-it/src/app/profile/public-profile/profile-modal/profile-modal.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-profile-modal',
   templateUrl: './profile-modal.component.html',
-  styleUrls: ['./profile-modal.component.css']
+  styleUrls: ['./profile-modal.component.css'],
 })
 export class ProfileModalComponent {
+  constructor() {}
+  /*
+  open(content: any) {
+    this.modalService.open(content);
+  }
 
+        @ViewChild("content",{static:true}) content:ElementRef;
+        ngOnInit(): void {
+            this.modalService.open(this.content);
+        }*/
 }
